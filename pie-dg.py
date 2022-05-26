1ef1ee7e7343ea410674c96001ab9196009e7772
theyanmay <103183852+theyanmay@users.noreply.github.com>
2022-05-26 15:04:09 +0300

    Update pie-dg.py

--- pie-dg.py
+++ pie-dg.py
@@ -8,29 +8,40 @@ import csv
 import matplotlib as mpl
 
 
-    dpi = 80
-    fig = plt.pie(dpi = dpi, figsize = (512 / dpi, 384 / dpi))
+def pie_figure(data: pd.DataFrame) -> plt.Figure:
+    from pandas.api.types import is_numeric_dtype
+    
+
+    fig1, ax1 = plt.subplots()
+    ax1.axis('equal')
+    ax1.legend(loc='upper left', bbox_to_anchor=(1.0, 1.0))
+
+    bins = 30
+    fig = plt.pie()
+    fig1 = fig
     data = data.dropna()
-    mpl.rcParams.update({'font.size': 9})
 
 
+    if not is_numeric_dtype(data.y.dtype):
+        raise ValueError(f'cannot draw bar plot for non-numeric series Y')
 
     if not is_numeric_dtype(data.x.dtype):
-        if data.x.nunique() > dpi:
-            raise ValueError(f'categorial column {data.x.name} has cardinality higher than {dpi}')
+        if data.x.nunique() > bins:
+            raise ValueError(f'categorial column {data.x.name} has cardinality higher than {bins}')
 
         data = data.groupby('x').mean().sort_values('y')
-        plt.pie(data.index, data.y, autopct='%.1f', radius = 1.1)
+        plt.pie(data.index, data.y)
+        plt.tight_layout()
         return fig
 
     data_range = (data.x.max() - data.x.min())
 
-    partitions = ((data.x - data.x.min()) / data_range * dpi).apply(np.floor)
+    partitions = ((data.x - data.x.min()) / data_range * bins).apply(np.floor)
     data = data.groupby(by=partitions).mean()
 
-    x, y = data.index * data_range / dpi + data.x.min(), data.y
+    x, y = data.index * data_range / bins + data.x.min(), data.y
 
-    bin_size = data_range / dpi / 2
+    bin_size = data_range / bins / 2
     plt.pie(x, y, width=bin_size)
 
     return fig
