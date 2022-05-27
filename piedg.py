9c9f5b3ca27aebb2b193610b548d73750c17e29e
theyanmay <103183852+theyanmay@users.noreply.github.com>
2022-05-27 14:07:15 +0300

    Update piedg.py

---
+++ piedg.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import pandas.api.types
+
+def plot_pie(data: pd.DataFrame) -> plt.Figure:
+
+    if not pandas.api.types.is_numeric_dtype(data.y.dtype):
+        raise ValueError(f'cannot draw pie for non-numeric series Y')
+
+    labels = data.x.values
+    sizes = data.y.values
+
+    figure, axis = plt.subplots()
+    axis.pie(sizes, labels=labels)
+    axis.axis('equal')
+
+    return figure
